a8582b59cd4eeb4b0bd1c84b40177dceb59cef99
Kararasenok =) <95347545+kararasenok-gd@users.noreply.github.com>
2023-10-29 14:20:38 +0300

    Изменение песни

--- idk/media.js
+++ idk/media.js
@@ -26,7 +26,7 @@ wavesurfer.init({
 
 
 //загружаем нужную песню
-wavesurfer.load("sound.mp3");
+wavesurfer.load("../audio/littlebiglobby.mp3");
 
 //play and pause a player
 playPause.addEventListener("click", function (e) {
