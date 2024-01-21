c860b8d0c6beffe24188e98eae96bf92f8616446
Kararasenok =) <95347545+kararasenok-gd@users.noreply.github.com>
2024-01-21 14:34:36 +0300

    JavaScript

--- js/js.js
+++ js/js.js
@@ -1,1 +1,31 @@
-document.getElementById("a").style.display = "block"
+function deleteAccountConfirmation() {
+    // var confirmationMessages = [
+    //     "Вы хотите удалить аккаунт?",
+    //     "Уверены?",
+    //     "ТОЧНО?",
+    //     "ЕСЛИ ВЫ УДАЛИТЕ АККАУНТ НИЧЕГО НЕ ОСТАНЕТСЯ! ВЫ УВЕРЕНЫ?",
+    //     "Нажмите ОК, чтобы удалить аккаунт"
+    // ];
+
+    // function showNextConfirmation(index) {
+    //     if (index >= confirmationMessages.length) {
+    //         alert("Удалено!")
+    //     } else {
+    //         var result = confirm(confirmationMessages[index]);
+    //         if (result) {
+    //             showNextConfirmation(index + 1);
+    //         }
+    //     }
+    // }
+
+    // showNextConfirmation(0);
+
+    var a = prompt("Text")
+    if(a === "") {
+        a = "Text"
+    }
+    document.getElementById("delAcc").innerHTML = a
+}
+
+
+document.getElementById("delAcc").addEventListener("click", deleteAccountConfirmation);
