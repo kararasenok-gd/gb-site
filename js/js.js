45b9800ab0ba2842e3d0961f9cdcaba8bd071eaf
Kararasenok =) <95347545+kararasenok-gd@users.noreply.github.com>
2024-02-11 14:35:39 +0300

    ы

--- js/js.js
+++ js/js.js
@@ -27,5 +27,44 @@ function deleteAccountConfirmation() {
     document.getElementById("delAcc").innerHTML = a
 }
 
+function a() {
+    var a = prompt("Оценка");
+    var b = "";
+    if (a === "1" || a === "2" || a === "3" || a === "4" || a === "5") {
+        if (a === "1" || a === "2" || a === "3" ) {
+            b = "плохая";
+        } else {
+            b = "хорошая";
+        }
+        
+        document.getElementById("ocenk").innerHTML = "Оценка: " + a;
+        document.getElementById("res").innerHTML = "Это " + b + " оценка";
+    } else {}
+}
 
-document.getElementById("delAcc").addEventListener("click", deleteAccountConfirmation);
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("delAcc").addEventListener("click", deleteAccountConfirmation);
+    document.getElementById("ocenk").addEventListener("click", a);
+});
+
+console.log(NaN + "5")
+
+const svetaScores = [40, 10, 40];
+const maximScores = [5, 40, 0];
+const dimaScores = [40, 1, 40];
+
+const svetaAvg = svetaScores.reduce((acc, score) => acc + score, 0) / svetaScores.length;
+const maximAvg = maximScores.reduce((acc, score) => acc + score, 0) / maximScores.length;
+const dimaAvg = dimaScores.reduce((acc, score) => acc + score, 0) / dimaScores.length;
+
+if (svetaAvg > maximAvg && svetaAvg > dimaAvg) {
+    console.log("Победитель: Света");
+} else if (maximAvg > svetaAvg && maximAvg > dimaAvg) {
+    console.log("Победитель: Максим");
+} else if (dimaAvg > svetaAvg && dimaAvg > maximAvg) {
+    console.log("Победитель: Дима");
+} else {
+    console.log("Тут больше чем 1 победитель xd");
+}
+
+
